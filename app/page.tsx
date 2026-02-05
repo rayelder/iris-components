ef1e4e0f030b882e794e1567972858377527c185
Ray Elder <hello@rayelder.com>
2026-02-05 16:01:31 -0700

    Add isPill property to Button

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,27 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.form}>
-      <h2>Add payment method</h2>
-      <InputText label="Card holder name" value="" />
-      <InputText label="Card number" value="" />
-      <div className={styles.row}>
-        <InputText label="Expiration date" value="" />
-        <InputText label="Security code" value="" />
+    <>
+      <div className={styles.form}>
+        <h2>Add payment method</h2>
+        <InputText label="Card holder name" value="" />
+        <InputText label="Card number" value="" />
+        <div className={styles.row}>
+          <InputText label="Expiration date" value="" />
+          <InputText label="Security code" value="" />
+        </div>
+        <Button label="Add payment method" />
       </div>
-      <Button label="Add payment method" />
-    </div>
+      <div className={styles.form}>
+        <h2>Add payment method</h2>
+        <InputText label="Card holder name" value="" />
+        <InputText label="Card number" value="" />
+        <div className={styles.row}>
+          <InputText label="Expiration date" value="" />
+          <InputText label="Security code" value="" />
+        </div>
+        <Button label="Add payment method" isPill />
+      </div>
+    </>
   );
 }
--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -2,8 +2,12 @@ import styles from "./Button.module.css";
 
 export default function Button({
   label = "Call-to-action",
+  isPill = false,
 }: {
   label: string;
+  isPill?: boolean;
 }) {
-  return <button className={styles.base}>{label}</button>;
+  return (
+    <button className={isPill ? styles.pill : styles.rounded}>{label}</button>
+  );
 }
