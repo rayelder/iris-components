53d640a2a452d28d2ddc4b23b29fc24420e5e7d5
Ray Elder <hello@rayelder.com>
2026-02-05 15:46:52 -0700

    Update preview page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
+import Button from "@/components/Button";
 import InputText from "@/components/InputText";
+
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.form}>
       <h2>Add payment method</h2>
       <InputText label="Card holder name" value="" />
       <InputText label="Card number" value="" />
-      <InputText label="Expiration date" value="" />
-      <InputText label="Security code" value="" />
+      <div className={styles.row}>
+        <InputText label="Expiration date" value="" />
+        <InputText label="Security code" value="" />
+      </div>
+      <Button label="Add payment method" />
     </div>
   );
 }
