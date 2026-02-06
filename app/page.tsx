8ae448f6ac75591701568c0ceeecca1554673f88
Ray Elder <hello@rayelder.com>
2026-02-06 10:06:14 -0700

    Update state styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,30 @@
 import Button from "@/components/Button";
+import InputSecurityCode from "@/components/InputSecurityCode";
 import InputText from "@/components/InputText";
 
 import styles from "./page.module.css";
+import Checkbox from "@/components/Checkbox";
 
 export default function Home() {
   return (
     <>
-      <div className={styles.form}>
+      <form className={styles.form}>
         <h2>Add payment method</h2>
+        <InputText label="Card number" value="" disabled />
         <InputText label="Card holder name" value="" />
         <InputText label="Card number" value="" />
         <div className={styles.row}>
           <InputText label="Expiration date" value="" />
-          <InputText label="Security code" value="" />
+          <InputSecurityCode label="Security code" value="" />
         </div>
+        <div className={styles.row}>
+          <InputText label="Expiration date" value="" />
+          <InputSecurityCode label="Security code" value="" />
+        </div>
+        <Checkbox label="Set as default payment method" />
+        <Checkbox label="Set as default payment method" />
         <Button label="Add payment method" />
-      </div>
-      <div className={styles.form}>
-        <h2>Add payment method</h2>
-        <InputText label="Card holder name" value="" />
-        <InputText label="Card number" value="" />
-        <div className={styles.row}>
-          <InputText label="Expiration date" value="" />
-          <InputText label="Security code" value="" />
-        </div>
-        <Button label="Add payment method" isPill />
-      </div>
+      </form>
     </>
   );
 }
--- components/InputText/index.tsx
+++ components/InputText/index.tsx
@@ -8,10 +8,16 @@ import styles from "./InputText.module.css";
 interface InputTextProps {
   label: string;
   value: string;
+  disabled?: boolean;
   onChange?: (value: string) => void;
 }
 
-export default function InputText({ label, value, onChange }: InputTextProps) {
+export default function InputText({
+  label,
+  value,
+  disabled = false,
+  onChange,
+}: InputTextProps) {
   const [isFocused, setIsFocused] = useState(false);
   const [inputValue, setInputValue] = useState(value);
 
@@ -31,7 +37,8 @@ export default function InputText({ label, value, onChange }: InputTextProps) {
         onChange={handleChange}
         onFocus={() => setIsFocused(true)}
         onBlur={() => setIsFocused(false)}
-        className={styles.input}
+        className={disabled ? styles.disabled : styles.input}
+        disabled={disabled}
       />
       <label
         className={`${styles.label} ${isLabelFloating ? styles.labelFloating : ""}`}
