25751eb0695b1197b69d64afe028f43527bda6e5
Ray Elder <hello@rayelder.com>
2026-02-20 11:08:34 -0700

    Stub out express checkout page

---
+++ app/express-checkout/page.tsx
@@ -0,0 +1,18 @@
+import Masthead from "@/components/Masthead";
+import OrderSummary from "@/components/OrderSummary";
+
+import style from "./page.module.css";
+
+export default function ExpressCheckout() {
+  return (
+    <div className={style.mobile}>
+      <div className={style.desktop}>
+        <Masthead />
+        <div className={style.padded}>
+          <h1>Express checkout</h1>
+          <OrderSummary />
+        </div>
+      </div>
+    </div>
+  );
+}
